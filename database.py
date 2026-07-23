ec58097138e012a774d4f35c0acb8082e85690a2
alia-77 <aliaahmadamin@gmail.com>
2026-07-23 23:48:02 +0300

    Migrate to Neon cloud database

--- database.py
+++ database.py
@@ -1,17 +1,26 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./securevault.db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
+
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    pool_pre_ping=True
+
 )
 
 SessionLocal = sessionmaker(
+
     autocommit=False,
     autoflush=False,
     bind=engine
+
 )
 
 Base = declarative_base()
